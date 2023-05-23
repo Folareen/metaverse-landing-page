fe17ee12a3a4fbf3a4b399e23657c496e9f38bb6
Folareen <sakawahab03@gmail.com>
2023-05-23 20:21:20 +0100

    added metaverse icon

---
+++ components/MetaverseIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MetaverseIcon = () => {
+    return (
+        <img src='/metaverse-effect-title-icon.png' className='metaverse-icon' />
+    )
+}
+
+export default MetaverseIcon
